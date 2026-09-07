522f644b835298f2a072a13d700f8a01adcd3241
oijwef098234 <oijwef098234@gmail.com>
2026-09-07 20:13:41 +0900

    fix(#131) :: 쿼리 생성 실패를 일으키던 법정지정분류 조회 메서드 선언 제거

--- src/main/java/com/command/toyvillage_server/domain/app/animal_manage/domain/repository/AnimalLegalStatusRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/animal_manage/domain/repository/AnimalLegalStatusRepository.java
@@ -3,8 +3,5 @@ package com.command.toyvillage_server.domain.app.animal_manage.domain.repository
 import com.command.toyvillage_server.domain.app.animal_manage.domain.AnimalLegalStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface AnimalLegalStatusRepository extends JpaRepository<AnimalLegalStatus, Long> {
-    List<AnimalLegalStatus> findAllById(List<Long> animalLegalStatusIds);
 }
