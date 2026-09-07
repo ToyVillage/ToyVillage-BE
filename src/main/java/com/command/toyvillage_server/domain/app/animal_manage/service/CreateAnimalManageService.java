48f66d1e492598a452da03a3fa3fd0cbf3b764a5
oijwef098234 <oijwef098234@gmail.com>
2026-09-07 14:55:04 +0900

    feat(#131) :: 동물 객체 생성 메서드 구현

--- src/main/java/com/command/toyvillage_server/domain/app/animal_manage/service/CreateAnimalManageService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/animal_manage/service/CreateAnimalManageService.java
@@ -2,6 +2,7 @@ package com.command.toyvillage_server.domain.app.animal_manage.service;
 
 import com.command.toyvillage_server.domain.app.animal_manage.domain.AnimalManage;
 import com.command.toyvillage_server.domain.app.animal_manage.domain.repository.AnimalManageRepository;
+import com.command.toyvillage_server.domain.app.animal_manage.presentation.dto.request.CreateAnimalManageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,5 +13,15 @@ public class CreateAnimalManageService {
     private final AnimalManageRepository animalManageRepository;
 
     @Transactional
-    public void execute() {}
+    public void execute(CreateAnimalManageRequest request) {
+        AnimalManage animalManage = AnimalManage.builder()
+            .animalName(request.animalName())
+            .animalGender(request.animalGender())
+            .birthYear(request.birthYear())
+            .otherInfo(request.otherInfo())
+            .animalImage(request.animalImage())
+            .build();
+
+        animalManageRepository.save(animalManage);
+    }
 }
