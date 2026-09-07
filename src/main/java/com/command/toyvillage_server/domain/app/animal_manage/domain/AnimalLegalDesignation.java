56e106839a0ba18bf10d0d21f0c8d03f9e956098
oijwef098234 <oijwef098234@gmail.com>
2026-09-07 12:58:25 +0900

    feat(#131) :: 동물관리와 법정지정분류 연결 엔티티 추가

---
+++ src/main/java/com/command/toyvillage_server/domain/app/animal_manage/domain/AnimalLegalDesignation.java
@@ -0,0 +1,24 @@
+package com.command.toyvillage_server.domain.app.animal_manage.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "tbl_animal_legal_designation")
+public class AnimalLegalDesignation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "animal_legal_status_id", nullable = false)
+    private AnimalLegalStatus animalLegalStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "animal_manage_id", nullable = false)
+    private AnimalManage animalManage;
+}
