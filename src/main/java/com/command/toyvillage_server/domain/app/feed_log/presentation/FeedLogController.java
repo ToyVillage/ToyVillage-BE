c4dd61af15a97ae8e59e50a2bd561d9966b24049
Kanghee <kh63167574@gmail.com>
2026-09-07 09:16:15 +0900

    feat(#134) :: 먹이급여일지 수정 기능 생성 기능 추가

--- src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/FeedLogController.java
+++ src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/FeedLogController.java
@@ -1,10 +1,32 @@
 package com.command.toyvillage_server.domain.app.feed_log.presentation;
 
+import com.command.toyvillage_server.domain.app.feed_log.presentation.dto.request.FeedLogRequest;
+import com.command.toyvillage_server.domain.app.feed_log.service.FeedLogCreateService;
+import com.command.toyvillage_server.domain.app.feed_log.service.FeedLogUpdateService;
+import com.command.toyvillage_server.global.common.response.MessageResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/feed-log")
 public class FeedLogController {
+    private final FeedLogCreateService feedLogCreateService;
+    private final FeedLogUpdateService feedLogUpdateService;
 
+    @PostMapping
+    public void createFeedLog(@RequestBody @Valid FeedLogRequest feedLogRequest) {
+        feedLogCreateService.execute(feedLogRequest);
+    }
+
+    @PutMapping("/{feed-log-id}")
+    public ResponseEntity<MessageResponse> updateFeedLog(
+            @PathVariable("feed-log-id") Long feedLogId,
+            @RequestBody @Valid FeedLogRequest request
+    ) {
+        feedLogUpdateService.execute(feedLogId, request);
+        return ResponseEntity.ok(MessageResponse.of("급여일지가 수정되었습니다."));
+    }
 }
