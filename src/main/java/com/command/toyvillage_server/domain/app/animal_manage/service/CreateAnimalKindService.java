9b24d8b0bb42b82d3fbb8a1c240a61f7ee9df7e8
oijwef098234 <oijwef098234@gmail.com>
2026-09-08 20:28:35 +0900

    fix(#131) :: 종 생성 법정지정분류 검증 추가

--- src/main/java/com/command/toyvillage_server/domain/app/animal_manage/service/CreateAnimalKindService.java
+++ src/main/java/com/command/toyvillage_server/domain/app/animal_manage/service/CreateAnimalKindService.java
@@ -27,7 +27,7 @@ public class CreateAnimalKindService {
 
     @Transactional
     public void execute(AnimalKindRequest request) {
-        List<AnimalLegalStatus> animalLegalStatuses = animalLegalStatusRepository.findAllById(request.animalLegalDesignation());
+        List<AnimalLegalStatus> animalLegalStatuses = findAnimalLegalStatuses(request.animalLegalDesignation());
 
         AnimalKind animalKind = AnimalKind.builder()
             .kindName(request.animalName())
@@ -49,5 +49,15 @@ public class CreateAnimalKindService {
             .toList();
 
         animalLegalDesignationRepository.saveAll(animalLegalDesignations);
+    }
+
+    private List<AnimalLegalStatus> findAnimalLegalStatuses(List<Long> animalLegalStatusIds) {
+        List<AnimalLegalStatus> animalLegalStatuses = animalLegalStatusRepository.findAllById(animalLegalStatusIds);
+
+        if (animalLegalStatuses.size() != new HashSet<>(animalLegalStatusIds).size()) {
+            throw AnimalLegalStatusNotFoundException.EXCEPTION;
+        }
+
+        return animalLegalStatuses;
     }
 }
