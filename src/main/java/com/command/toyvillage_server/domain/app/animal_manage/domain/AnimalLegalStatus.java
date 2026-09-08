910ef1a494054759bb838dac7a459f0ed73d10cc
oijwef098234 <oijwef098234@gmail.com>
2026-09-08 09:39:32 +0900

    feat(#131) :: 법정지정분류 수정 기능 구현

--- src/main/java/com/command/toyvillage_server/domain/app/animal_manage/domain/AnimalLegalStatus.java
+++ src/main/java/com/command/toyvillage_server/domain/app/animal_manage/domain/AnimalLegalStatus.java
@@ -16,4 +16,8 @@ public class AnimalLegalStatus {
 
     @Column(name = "kind", nullable = false)
     private String kind;
+
+    public void update(String kind) {
+        this.kind = kind;
+    }
 }
