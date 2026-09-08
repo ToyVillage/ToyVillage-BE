11b9440a9252442eff912eb0c37a7e4ed6f12a57
oijwef098234 <oijwef098234@gmail.com>
2026-09-08 16:15:56 +0900

    style(#131) :: import 블록 공백 제거

--- src/main/java/com/command/toyvillage_server/domain/app/animal_manage/presentation/dto/request/AnimalKindRequest.java
+++ src/main/java/com/command/toyvillage_server/domain/app/animal_manage/presentation/dto/request/AnimalKindRequest.java
@@ -4,7 +4,6 @@ import com.command.toyvillage_server.domain.app.animal_manage.domain.enums.Anima
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 import java.util.List;
 
 public record AnimalKindRequest(
