1b7d866ad26e2671a342aa040bf37f2f20e1a2ae
treegus <cowtree28@gmail.com>
2026-09-09 18:58:58 +0900

    feat(#124) :: 팀 배정 해제 API 추가

--- src/main/java/com/command/toyvillage_server/domain/app/join_team/presentation/JoinTeamController.java
+++ src/main/java/com/command/toyvillage_server/domain/app/join_team/presentation/JoinTeamController.java
@@ -1,9 +1,11 @@
 package com.command.toyvillage_server.domain.app.join_team.presentation;
 
 import com.command.toyvillage_server.domain.app.join_team.service.JoinTeamService;
+import com.command.toyvillage_server.domain.app.join_team.service.LeaveTeamService;
 import com.command.toyvillage_server.global.common.response.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class JoinTeamController {
     private final JoinTeamService joinTeamService;
+    private final LeaveTeamService leaveTeamService;
 
     @PutMapping("/{appAdminId}/{teamId}")
     public ResponseEntity<MessageResponse> joinTeam(
@@ -24,6 +27,15 @@ public class JoinTeamController {
 
         return ResponseEntity.ok(
                 MessageResponse.of("앱 관리자가 팀에 배정되었습니다.")
+        );
+    }
+
+    @DeleteMapping("/{appAdminId}")
+    public ResponseEntity<MessageResponse> leaveTeam(@PathVariable Long appAdminId) {
+        leaveTeamService.execute(appAdminId);
+
+        return ResponseEntity.ok(
+                MessageResponse.of("팀 배정이 해제되었습니다.")
         );
     }
 }
