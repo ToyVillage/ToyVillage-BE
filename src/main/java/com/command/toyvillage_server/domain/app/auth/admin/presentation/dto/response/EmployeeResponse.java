449a9f9d5e473ab453537d1764b1fe1999e5fc99
treegus <cowtree28@gmail.com>
2026-09-09 18:58:11 +0900

    feat(#124) :: 직원 직급 필드 추가

--- src/main/java/com/command/toyvillage_server/domain/app/auth/admin/presentation/dto/response/EmployeeResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/auth/admin/presentation/dto/response/EmployeeResponse.java
@@ -7,13 +7,15 @@ import lombok.Builder;
 public record EmployeeResponse(
         Long id,
         String username,
-        String name
+        String name,
+        String position
 ) {
     public static EmployeeResponse from(AppAdmin employee) {
         return EmployeeResponse.builder()
                 .id(employee.getId())
                 .username(employee.getUsername())
                 .name(employee.getName())
+                .position(employee.getPosition())
                 .build();
     }
 }
