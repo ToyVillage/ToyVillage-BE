119abe9e747eda267a794ac6cd05952209507983
Kanghee <kh63167574@gmail.com>
2026-09-10 16:54:48 +0900

    feat : feedlogadminqueryresponse 추가

--- src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/dto/response/FeedLogAdminDetailsQueryResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/dto/response/FeedLogAdminDetailsQueryResponse.java
@@ -1,4 +1,32 @@
 package com.command.toyvillage_server.domain.app.feed_log.presentation.dto.response;
 
-public record FeedLogAdminDetailsQueryResponse() {
+import com.command.toyvillage_server.domain.app.feed_log.domain.FeedLog;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+public record FeedLogAdminDetailsQueryResponse(
+        Long animalId,
+        String name,
+        String animalKind,
+        String animalName,
+        String feedType,
+        Integer feedAmount,
+        LocalDate feedDate,
+        LocalDateTime feedStartTime
+) {
+    public static FeedLogAdminDetailsQueryResponse from(FeedLog feedLog) {
+        return FeedLogAdminDetailsQueryResponse.builder()
+                .animalId(feedLog.getAnimalManage().getId())
+                .name(feedLog.getAppAdmin().getName())
+                .animalKind(feedLog.getAnimalManage().getAnimalKind().getKindName())
+                .animalName(feedLog.getAnimalManage().getAnimalName())
+                .feedType(feedLog.getFeedType())
+                .feedAmount(feedLog.getFeed_amount())
+                .feedDate(feedLog.getFeedDate())
+                .feedStartTime(feedLog.getFeedStartTime())
+                .build();
+    }
 }
