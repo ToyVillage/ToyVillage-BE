25e08ba97fca7726f3febe0fbff9a1352e1cd8e2
Kanghee <kh63167574@gmail.com>
2026-09-10 19:24:53 +0900

    refactor(#134) ::다른 직원의 기록을 조회·수정할 수 있던 문제 수정

--- src/main/java/com/command/toyvillage_server/domain/app/feed_log/domain/repository/FeedLogRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/feed_log/domain/repository/FeedLogRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface FeedLogRepository extends JpaRepository<FeedLog, Long> {
+
+    Optional<FeedLog> findByIdAndAppAdmin_Id(Long id, Long writerId);
 
     List<FeedLog> findAllByFeedDate(LocalDate feedDate);
 
