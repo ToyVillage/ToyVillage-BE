c7ada20b8a70b5bf6011ca929ff03700f93c5527
Kanghee <kh63167574@gmail.com>
2026-09-16 19:09:24 +0900

    refactor(#152) :: admin/feedid에 pageable추가  fcce3c

--- src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/dto/response/FeedLogAdminQueryListResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/dto/response/FeedLogAdminQueryListResponse.java
@@ -2,17 +2,20 @@ package com.command.toyvillage_server.domain.app.feed_log.presentation.dto.respo
 
 import com.command.toyvillage_server.domain.app.feed_log.domain.FeedLog;
 import lombok.Builder;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
 public record FeedLogAdminQueryListResponse(
-        List<FeedLogResponse> feedLogs
+        List<FeedLogResponse> feedLogs,
+        int totalPageSize
 ) {
-    public static FeedLogAdminQueryListResponse from(List<FeedLog> feedLogs) {
+    public static FeedLogAdminQueryListResponse from(Page<FeedLog> feedLogs) {
         return FeedLogAdminQueryListResponse.builder()
-                .feedLogs(feedLogs.stream().map(FeedLogResponse::from).toList())
+                .feedLogs(feedLogs.map(FeedLogResponse::from).toList())
+                .totalPageSize(feedLogs.getTotalPages())
                 .build();
     }
 
