1a9ec05bc968d9244d7909521cb7a9d31996a3dc
oijwef098234 <oijwef098234@gmail.com>
2026-09-16 15:09:14 +0900

    fix(#150) :: 템플릿 전체조회 부분 작성자 필드 추가

--- src/main/java/com/command/toyvillage_server/domain/app/work_log/domain/WorkLogTemplate.java
+++ src/main/java/com/command/toyvillage_server/domain/app/work_log/domain/WorkLogTemplate.java
@@ -1,15 +1,7 @@
 package com.command.toyvillage_server.domain.app.work_log.domain;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
-import jakarta.persistence.Table;
+import com.command.toyvillage_server.domain.app.auth.admin.domain.AppAdmin;
+import jakarta.persistence.*;
 import com.command.toyvillage_server.domain.app.work_log.exception.WorkLogAnswerRequiredException;
 import com.command.toyvillage_server.domain.app.work_log.exception.WorkLogQuestionNotFoundException;
 import com.command.toyvillage_server.domain.app.work_log.exception.WorkLogSectionNotFoundException;
@@ -57,6 +49,10 @@ public class WorkLogTemplate {
 
     @Column(name = "delete_yn", nullable = false)
     private boolean deleteYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "app_admin_id", nullable = false)
+    private AppAdmin appAdmin;
 
     @Builder
     private WorkLogTemplate(String templateTitle) {
