dbeac9be22e643b24cadcd6c3d2dfcdb8a053748
Kanghee <kh63167574@gmail.com>
2026-09-16 23:08:13 +0900

    Auto stash before checking out "origin/develop"

--- src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/dto/response/FeedLogAdminSalaryHistoryListQueryResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/feed_log/presentation/dto/response/FeedLogAdminSalaryHistoryListQueryResponse.java
@@ -3,7 +3,7 @@ package com.command.toyvillage_server.domain.app.feed_log.presentation.dto.respo
 import com.command.toyvillage_server.domain.app.feed_log.domain.FeedLog;
 import lombok.Builder;
 
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
@@ -22,7 +22,7 @@ public record FeedLogAdminSalaryHistoryListQueryResponse(
             String staffName,
             String feedType,
             Float feedAmount,
-            OffsetDateTime feedDateTime,
+            LocalDateTime feedDateTime,
             String significant
     ) {
         public static FeedLogResponse from(FeedLog feedLog) {
@@ -31,7 +31,7 @@ public record FeedLogAdminSalaryHistoryListQueryResponse(
                     .staffName(feedLog.getAppAdmin().getName())
                     .feedType(feedLog.getFeedType())
                     .feedAmount(feedLog.getFeedAmount())
-                    .feedDateTime(feedLog.getFeedDateTimeKst())
+                    .feedDateTime(feedLog.getFeedDateTime())
                     .significant(feedLog.getSignificant())
                     .build();
         }
