ca12890ff07448280c5564972c222af091b76793
Kanghee <kh63167574@gmail.com>
2026-09-17 22:13:35 +0900

    hotfix :: 대시보드 개체관리에 id 추가

--- src/main/java/com/command/toyvillage_server/domain/app/dashboard/presentation/dto/response/DashBoardQueryAnimalManageResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/dashboard/presentation/dto/response/DashBoardQueryAnimalManageResponse.java
@@ -1,5 +1,6 @@
 package com.command.toyvillage_server.domain.app.dashboard.presentation.dto.response;
 
+import com.command.toyvillage_server.domain.app.animal_manage.domain.AnimalManage;
 import com.command.toyvillage_server.domain.app.animal_manage.domain.AnimalObservation;
 import lombok.Builder;
 
@@ -8,12 +9,14 @@ import java.time.LocalDateTime;
 @Builder
 public record DashBoardQueryAnimalManageResponse(
         Long animalObservationId,
+        Long animalId,
         String title,
         LocalDateTime createdAt
 ) {
-    public static DashBoardQueryAnimalManageResponse from(AnimalObservation animalObservation) {
+    public static DashBoardQueryAnimalManageResponse of(AnimalObservation animalObservation, AnimalManage animalManage) {
         return DashBoardQueryAnimalManageResponse.builder()
                 .animalObservationId(animalObservation.getId())
+                .animalId(animalManage.getId())
                 .title(animalObservation.getTitle())
                 .createdAt(animalObservation.getCreatedAt())
                 .build();
