fc1e1ca091dfd5cc003b06fe7c9daf1d7f98e2ef
oijwef098234 <oijwef098234@gmail.com>
2026-09-25 02:07:17 +0900

    refactor(#191) :: 활성 직원 조회 메서드 추가

--- src/main/java/com/command/toyvillage_server/domain/app/auth/admin/domain/repository/AppAdminRepository.java
+++ src/main/java/com/command/toyvillage_server/domain/app/auth/admin/domain/repository/AppAdminRepository.java
@@ -10,9 +10,23 @@ import java.util.Optional;
 public interface AppAdminRepository extends JpaRepository<AppAdmin, Long> {
     Optional<AppAdmin> findByUsername(String username);
 
+    Optional<AppAdmin> findByUsernameAndDeleteStatusFalse(String username);
+
+    Optional<AppAdmin> findByIdAndDeleteStatusFalse(Long id);
+
+    Optional<AppAdmin> findByIdAndRoleAndDeleteStatusFalse(Long id, AppAdminRole role);
+
+    List<AppAdmin> findAllByIdInAndDeleteStatusFalse(List<Long> ids);
+
+    boolean existsByIdAndDeleteStatusFalse(Long id);
+
     boolean existsByUsername(String username);
 
     List<AppAdmin> findByRoleOrderByNameAsc(AppAdminRole role);
 
+    List<AppAdmin> findByRoleAndDeleteStatusFalseOrderByNameAsc(AppAdminRole role);
+
     List<AppAdmin> findAllByRoleOrderByIdAsc(AppAdminRole role);
+
+    List<AppAdmin> findAllByRoleAndDeleteStatusFalseOrderByIdAsc(AppAdminRole role);
 }
