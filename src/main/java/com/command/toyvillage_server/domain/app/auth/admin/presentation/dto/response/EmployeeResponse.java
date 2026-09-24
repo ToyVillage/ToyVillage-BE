d1530559f12ffd5d6cdb62625814eb43c8dbcf29
oijwef098234 <oijwef098234@gmail.com>
2026-09-25 02:07:17 +0900

    feat(#191) :: 직원 전체 조회 응답에 가입일 반영

--- src/main/java/com/command/toyvillage_server/domain/app/auth/admin/presentation/dto/response/EmployeeResponse.java
+++ src/main/java/com/command/toyvillage_server/domain/app/auth/admin/presentation/dto/response/EmployeeResponse.java
@@ -3,19 +3,21 @@ package com.command.toyvillage_server.domain.app.auth.admin.presentation.dto.res
 import com.command.toyvillage_server.domain.app.auth.admin.domain.AppAdmin;
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 public record EmployeeResponse(
         Long id,
         String username,
         String name,
-        String position
+        LocalDate createAt
 ) {
     public static EmployeeResponse from(AppAdmin employee) {
         return EmployeeResponse.builder()
                 .id(employee.getId())
                 .username(employee.getUsername())
                 .name(employee.getName())
-                .position(employee.getPosition())
+                .createAt(employee.getCreateAt())
                 .build();
     }
 }
